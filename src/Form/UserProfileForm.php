78693d05c218e7bcd49b097bb400ce54bf57e987
Ivan Stanojevic <ivanstan@gmail.com>
2020-04-11 18:01:34 +0200

    add first and last name

--- src/Form/UserProfileForm.php
+++ src/Form/UserProfileForm.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,9 +12,30 @@ class UserProfileForm extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('preference', UserPreferenceForm::class, [
-            'label' => false
-        ]);
+        $builder
+            ->add(
+                'firstName',
+                TextType::class,
+                [
+                    'label' => false,
+                    'attr' => ['placeholder' => 'First name']
+                ]
+            )
+            ->add(
+                'lastName',
+                TextType::class,
+                [
+                    'label' => false,
+                    'attr' => ['placeholder' => 'Last name']
+                ]
+            )
+            ->add(
+                'preference',
+                UserPreferenceForm::class,
+                [
+                    'label' => false,
+                ]
+            );
     }
 
     public function configureOptions(OptionsResolver $resolver): void
