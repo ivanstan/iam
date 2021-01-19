9406c81b463258ec93571653943f200549ce5bdb
Ivan Stanojevic <ivanstan@gmail.com>
2021-01-19 19:15:02 +0100

    add panther

---
+++ tests/Panther/DefaultTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests\Panther;
+
+use Symfony\Component\Panther\PantherTestCase;
+
+class DefaultTest extends PantherTestCase
+{
+    public function testHomePage(): void {
+        $client = static::createPantherClient();
+        $client->request('GET', '/');
+    }
+}
